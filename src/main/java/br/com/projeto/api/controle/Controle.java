35ef89cc200cf16f0bd674ca318268babc99f2f8
Thales Spanhol <thales_spanhol@hotmail.com>
2024-07-20 17:25:15 -0300

    PathVariable

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,13 @@ public class Controle {
         return "Hello World!";
     }
 
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Seja vem vindo(a)";
+    }
+
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome){
+        return "Seja vem vindo(a) " + nome + "!";
+    }
 }
