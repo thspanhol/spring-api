44080f5fda75ed840a9c451077a4cbd0d801f97f
Thales Spanhol <thales_spanhol@hotmail.com>
2024-07-19 23:20:50 -0300

    first commit

---
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -0,0 +1,14 @@
+package br.com.projeto.api.controle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controle {
+    
+    @GetMapping("")
+    public String mensagem(){
+        return "Hello World!";
+    }
+
+}
