d3173f1b9640b1bcc405c23570527d8da30a8501
Thales Spanhol <thales_spanhol@hotmail.com>
2024-07-27 21:02:40 -0300

    Autowired

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,5 +1,6 @@
 package br.com.projeto.api.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Pessoa;
+import br.com.projeto.api.repositorio.Repositorio;
 
 @RestController
 public class Controle {
     
+    @Autowired
+    private Repositorio acao;
+
     @GetMapping("")
     public String mensagem(){
         return "Hello World!";
