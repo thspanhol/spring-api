81fda8550918e4439bebf506cba47131ba11356d
Thales Spanhol <thales_spanhol@hotmail.com>
2024-09-01 21:14:15 -0300

    Annotations para validar dados

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.api.modelo.Cliente;
 import br.com.projeto.api.modelo.Pessoa;
 import br.com.projeto.api.repositorio.Repositorio;
 import br.com.projeto.api.servico.Servico;
+import jakarta.validation.Valid;
 
 @RestController
 public class Controle {
@@ -115,4 +117,10 @@ public class Controle {
     public ResponseEntity<?> status() {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @PostMapping("/cliente")
+    public void cliente(@Valid @RequestBody Cliente obj){
+
+    }
+
 }
