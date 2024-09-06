67470c72c92cfcccd0968a503e4bf950cac2a27a
Thales Spanhol <thales_spanhol@hotmail.com>
2024-09-05 22:55:17 -0300

    especifica erro

--- src/main/java/br/com/projeto/api/repositorio/Repositorio.java
+++ src/main/java/br/com/projeto/api/repositorio/Repositorio.java
@@ -10,7 +10,7 @@ import br.com.projeto.api.modelo.Pessoa;
 
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer> {
-    
+
     List<Pessoa> findAll();
 
     Pessoa findByCodigo(int codigo);
