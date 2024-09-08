150df07204b81aa462a51cb27e9fc36bb52c9b33
Thales Spanhol <thales_spanhol@hotmail.com>
2024-09-08 20:53:07 -0300

    especifica erro

--- src/main/java/br/com/projeto/api/modelo/Cliente.java
+++ src/main/java/br/com/projeto/api/modelo/Cliente.java
@@ -27,20 +27,25 @@ public class Cliente {
     public int getCodigo() {
         return codigo;
     }
+
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
 }
